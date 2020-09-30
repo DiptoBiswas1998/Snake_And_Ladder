a51618ae073a4665d699129cd3765a0dd515d91d
Dipto <diptobiswas1998@gmail.com>
2020-10-01 01:23:39 +0530

    [Dipto] refactor: Adding the starting point of the game

--- Snake_And_Ladder/Snake_And_Ladder/Program.cs
+++ Snake_And_Ladder/Snake_And_Ladder/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
-namespace Snake_And_Ladder
+namespace SnakeAndLadderSimulator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Welcome msg
             Console.WriteLine("Welcome to the Snake and Ladder Problem!");
+            //Initialising
+            int currentPosition = 0;
+            Console.WriteLine("Starting position = "+ currentPosition);
         }
     }
 }
