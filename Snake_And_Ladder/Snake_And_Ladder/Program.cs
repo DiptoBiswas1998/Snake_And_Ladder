2c4637715a5481e2bbec2c6ddb7b295e2ba37132
Dipto <diptobiswas1998@gmail.com>
2020-10-01 07:52:42 +0530

    [Dipto] refactor: Making changes

--- Snake_And_Ladder/Snake_And_Ladder/Program.cs
+++ Snake_And_Ladder/Snake_And_Ladder/Program.cs
@@ -6,6 +6,8 @@ namespace SnakeAndLadderSimulator
         public const int NoPlay = 0;
         public const int Ladder = 1;
         public const int Snake = 2;
+        public const int WinPosition = 100;
+        public const int StartPosition = 0;
         //Returns Output Of Die Roll
         public static int rollTheDie()
         {
@@ -38,7 +40,7 @@ namespace SnakeAndLadderSimulator
             Console.WriteLine("Welcome to the Snake and Ladder Problem!");
             int currentPosition = 0, nextPosition;
             int count = 0;
-            while (currentPosition < 100)
+            while (currentPosition < StartPosition)
             {
                 int numberOnDie = rollTheDie();
                 count++;
